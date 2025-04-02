d09aea1d41b81a04ab80ecff77e7aba7a6137b5c
qgymib <4520263+qgymib@users.noreply.github.com>
2025-04-02 10:38:14 +0800

    Add addon framework

--- src/addon/__init__.c
+++ src/addon/__init__.c
@@ -1,1 +1,34 @@
+#include "utils/defs.h"
+#include "utils/log.h"
 #include "__init__.h"
+
+static const nt_addon_factory_t* s_addons[] = {
+    &nt_addon_dns,
+};
+
+int nt_addon_make(nt_addon_t** addon, const char* url, int inbound, int outbound)
+{
+    url_comp_t* comp = NULL;
+    int         ret = nt_url_comp_parser(&comp, url);
+    if (ret != 0)
+    {
+        LOG_E("Url `%s` parser failed.", url);
+        return ret;
+    }
+
+    size_t i;
+
+    ret = NT_ERR(ENOTSUP);
+    for (i = 0; i < ARRAY_SIZE(s_addons); i++)
+    {
+        const nt_addon_factory_t* factory = s_addons[i];
+        if (strcmp(factory->scheme, comp->scheme) == 0)
+        {
+            ret = factory->make(addon, comp, inbound, outbound);
+            break;
+        }
+    }
+
+    nt_url_comp_free(comp);
+    return ret;
+}
--- src/addon/__init__.h
+++ src/addon/__init__.h
@@ -1,10 +1,50 @@
 #ifndef NT_ADDON_INIT_H
 #define NT_ADDON_INIT_H
+
+#include "utils/urlparser.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef struct nt_addon
+{
+    /**
+     * @brief Release this object.
+     * @param[in] thiz  Object handle.
+     */
+    void (*release)(struct nt_addon* thiz);
+} nt_addon_t;
 
+typedef struct nt_addon_factory
+{
+    /**
+     * @brief Addon scheme.
+     */
+    const char* scheme;
+
+    /**
+     * @brief Create function.
+     * @param[out] addon    Addon handle.
+     * @param[in] url       URL.
+     * @param[in] inbound   Inbound file descriptor.
+     * @param[in] outbound  Outbound file descriptor.
+     * @return 0 for success, or errno.
+     */
+    int (*make)(nt_addon_t** addon, const url_comp_t* url, int inbound, int outbound);
+} nt_addon_factory_t;
+
+extern const nt_addon_factory_t nt_addon_dns;
+
+/**
+ * @brief Create addon.
+ * @param[out] addon    Addon handle.
+ * @param[in] url       Creation URL.
+ * @param[in] inbound   Inbound file descriptor.
+ * @param[in] outbound  Outbound file descriptor.
+ * @return 0 if success, errno if failed.
+ */
+int nt_addon_make(nt_addon_t** addon, const char* url, int inbound, int outbound);
 
 #ifdef __cplusplus
 }
